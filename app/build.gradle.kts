403ea3fc5dae1c423a8a2e478ec7a7dee08ebe21
Wzindx <2753761311@qq.com>
2026-04-28 18:49:42 +0800

    build: bump version to 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.operit.hohyaiimage"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     buildTypes {
