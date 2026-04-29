974358f04e898c84add6c8209d59e860eff7abcc
Wzindx <2753761311@qq.com>
2026-04-29 19:15:11 +0000

    Add material-icons-extended dependency for dropdown icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,6 +74,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation("androidx.compose.material:material-icons-extended")
     implementation(libs.androidx.security.crypto)
     debugImplementation(libs.androidx.ui.tooling)
 }
