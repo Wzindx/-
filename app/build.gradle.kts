6aab191833640348f88c69cfcb3712c27b11c88b
Wzindx <2753761311@qq.com>
2026-04-30 04:26:58 +0000

    Migrate Kotlin jvmTarget DSL

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,4 @@
-plugins {
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget\n\nplugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
