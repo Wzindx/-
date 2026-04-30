0e90e5104a955b3192c17e7e5441551226547f2c
Wzindx <2753761311@qq.com>
2026-04-30 00:48:23 +0000

    Add width import for compact dropdown menus

--- app/src/main/java/com/yang/emperor/MainActivity.kt
+++ app/src/main/java/com/yang/emperor/MainActivity.kt
@@ -38,6 +38,7 @@ import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
